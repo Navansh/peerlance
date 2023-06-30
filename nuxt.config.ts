31ca14305333dbd3a1def9043de4914b45260834
Shalini <58693482+nashimocodes@users.noreply.github.com>
2023-06-30 20:39:47 +0530

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'opinionated nuxt',
+      meta: [
+        {
+          name: 'description',
+          content: 'an opinionated nuxt starter template',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          href: '/oink.svg',
+        },
+      ],
+    },
+  },
+
+  modules: ['@vueuse/nuxt', '@unocss/nuxt'],
+  css: [
+    '@unocss/reset/tailwind.css',
+    'vue-toast-notification/dist/theme-sugar.css',
+  ],
+  imports: {
+    imports: [
+      {
+        name: 'nanoid',
+        from: 'nanoid',
+      },
+    ],
+  },
+})
