45d547cf3bb1fd53d98008663f67d2b67d5cc0d2
Piyush <bhangalepiyush@gmail.com>
2023-07-01 20:10:50 +0530

    feat: initial sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <div>
-    <slot />
+  <div class="bg-[#14161b] font-sans text-white">
+    <div class="grid grid-cols-12 h-screen gap-4 py-4" overflow-y-hidden>
+      <div col-span-2>
+        <NavigationSidebar />
+      </div>
+
+      <div col-span-10>
+        <slot />
+      </div>
+    </div>
   </div>
 </template>
 
