020565494832f16ffabf41e66ddb9e821e80c7dd
Piyush <bhangalepiyush@gmail.com>
2023-07-01 23:45:53 +0530

    feat: add initial login design

---
+++ layouts/login.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div bg-center class="h-screen bg-[#14161b] bg-[url(/bg.svg)] bg-center bg-no-repeat">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
