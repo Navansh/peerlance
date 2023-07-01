85512568236257f10e51ea251dd35a6511acdc7a
Piyush <bhangalepiyush@gmail.com>
2023-07-02 01:32:06 +0530

    feat: don't connect if already connected

--- components/Hedera/Connect.client.vue
+++ components/Hedera/Connect.client.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import { isWalletAvailable } from '@/lib/hedera/wallet/store'
-import { hashconnect, initHashConnect } from '@/lib/hedera/wallet/init'
+import { isWalletAvailable, isWalletConnected, userAccountId } from '@/lib/hedera/wallet/store'
+import { connectToWallet, hashconnect, initHashConnect } from '@/lib/hedera/wallet/init'
 
 await initHashConnect()
-consola.withTag('[HEDERA]').info('init: ', hashconnect)
+consola.withTag('Hedera').info('init: ', hashconnect)
 </script>
 
 <template>
   <div>
-    <button v-if="isWalletAvailable" flex items-center gap-2 rounded-full bg-white px-4 py-2 text-black>
-      <div i-simple-icons-hedera />
-      Connect Hedera Wallet
-    </button>
+    <div v-if="isWalletAvailable">
+      <div v-if="isWalletConnected" flex items-center gap-2>
+        <span font-bold underline="~ double">Hedera Account ID:</span> {{ userAccountId }}
+      </div>
+      <button v-else flex items-center gap-2 rounded-full bg-white px-4 py-2 text-black @click="connectToWallet">
+        <div i-simple-icons-hedera />
+        Connect Hedera Wallet
+      </button>
+    </div>
 
     <div v-else flex items-center gap-2>
       <div i-fxemoji-warningsign />
