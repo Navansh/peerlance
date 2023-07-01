a51df31bce4d3df749a19b12564fb10aa0a2cc00
Piyush <bhangalepiyush@gmail.com>
2023-07-01 21:10:34 +0530

    refactor: convert div to nuxtlink in navitem

--- components/Navigation/NavItem.vue
+++ components/Navigation/NavItem.vue
@@ -7,10 +7,10 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-[#14161b]" flex items-center justify-start gap-2 rounded-xl px-6 py-6>
+  <NuxtLink :to="href" class="bg-[#14161b]" flex items-center justify-start gap-2 rounded-xl px-6 py-6>
     <div :class="icon" color-brand />
     {{ name }}
-  </div>
+  </NuxtLink>
 </template>
 
 <style scoped>
