4a60615d9465acaee90613c6c3a69c3b26a0a9df
Piyush <bhangalepiyush@gmail.com>
2023-07-02 01:49:55 +0530

    feat: add thumbnail image

---
+++ pages/projects/create.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div h-full flex flex-col gap-6>
+    <h1 text-2xl font-bold>
+      Add your Project, <span text-brand underline="~ wavy brand">Lancer</span>
+    </h1>
+
+    <div class="h-1/3 w-full rounded-xl bg-[#212328]">
+      <img h-full w-full rounded-xl object-cover src="https://cdn.discordapp.com/attachments/1112770406059880609/1114880464298192917/Thumbnail.png" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
