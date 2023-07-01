8fce13e229bc15d7705423eb6b9e876235799816
Piyush <bhangalepiyush@gmail.com>
2023-07-02 01:40:46 +0530

    refactor: increase gap between columns

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="bg-[#14161b] font-sans text-white">
-    <div class="grid grid-cols-12 h-screen gap-4 py-4" overflow-y-hidden>
+    <div class="grid grid-cols-12 h-screen gap-8 py-4" overflow-y-hidden>
       <div col-span-2>
         <NavigationSidebar />
       </div>
