7f78a11a79a6aed7cec1f589834527941e6edd54
Piyush <bhangalepiyush@gmail.com>
2023-07-02 17:28:55 +0530

    feat: updates

--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -4,11 +4,18 @@ export default defineEventHandler(async (event) => {
   const requestQuery = getQuery(event)
   const { userId } = requestQuery
 
-  const userData = await prisma.user.findFirst({
+  const userData = await prisma.user.findUnique({
     where: {
       userId: userId! as string,
     },
   })
+
+  if (!userData) {
+    throw createError({
+      statusCode: 404,
+      message: 'User not found',
+    })
+  }
 
   return userData
 })
