e12f5282560094f71a74ac6c06e523d70193135c
Piyush <bhangalepiyush@gmail.com>
2023-07-02 10:52:38 +0530

    feat: wait for auth before redirects

--- app.vue
+++ app.vue
@@ -1,11 +1,33 @@
 <script lang="ts" setup>
+import { getAuth } from 'firebase/auth'
 import app from '@/utils/firebase'
 
+const loading = ref(true)
+
 consola.withTag('INIT').info('Hello, Firebase!', app)
+
+onMounted(async () => {
+  const auth = getAuth()
+
+  auth.onAuthStateChanged((user) => {
+    consola.withTag('INIT').info('user: ', user)
+    if (user) {
+      loading.value = false
+      navigateTo('/')
+    }
+    else {
+      loading.value = false
+      navigateTo('/login')
+    }
+  })
+})
 </script>
 
 <template>
-  <NuxtLayout>
+  <div v-if="loading" flex items-center justify-center bg-center text-white class="font-poppins h-screen bg-[#14161b] bg-[url(/bg.svg)] bg-center bg-no-repeat">
+    <div i-eos-icons-loading h-12 w-12 />
+  </div>
+  <NuxtLayout v-else>
     <NuxtPage />
   </NuxtLayout>
 </template>
